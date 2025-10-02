08038f0aeb5d02b7113f68397a91104b95dba37b
VumFaw <faw@uz.com>
2025-10-02 12:36:06 +0500

    add some thing and fix

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,12 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
+    path('products/', views.products, name='products'),
+    path('become-a-dealer/', views.products, name='become_a_dealer'),
+    path('lizing/', views.products, name='lizing'),
+    path('news/', views.products, name='news'),
+    path('products/', views.products, name='products'),
+    path('dealers/', views.products, name='dealers'),
+    path('jobs/', views.products, name='jobs'),
 ]
